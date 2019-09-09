34c0670284d535799baed9a1f7b40c6c3672de2d
cismox owner <darshit.hasmukhbhai1998@gmail.com>
2019-09-10 02:34:49 +0530

    update2

--- App.js
+++ App.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { StyleSheet, Image, ImageBackground, Dimensions, TouchableOpacity, View, TextInput, ToastAndroid, Vibration, ScrollView } from 'react-native';
+import {
+  StyleSheet, Image, ImageBackground,
+  Dimensions,
+  TouchableOpacity, View, TextInput, ToastAndroid, Vibration, ScrollView
+} from 'react-native';
 import { AppLoading } from 'expo';
 import { Container, Root, Card, Text, CardItem, Content, Header, Left, Body, Right, Button, Icon, Title, Input, Item, Toast } from 'native-base';
 import * as Font from 'expo-font';
@@ -11,10 +15,13 @@ import { createBottomTabNavigator, createMaterialTopTabNavigator } from 'react-n
 import Navigation from './Navigator/Navigation';
 import Trip from './Navigator/Trips.js';
 import Saved from './Navigator/saved';
+import StarRating from 'react-native-star-rating';
 //flex 1 width the maximum te api te dese
 //background colot thi 80 height agad teno color e white te bani jase
 //but borderBottomWidth thi jya tame je height te 80 up che tya border te bani te jase
 //but when borderBottomColor thi border no color e chnage te thai jase
+let width = Dimensions.get('window').width;
+let height = Dimensions.get('window').height;
 class Explore extends React.Component {
   constructor(props) {
     super(props);
@@ -47,6 +54,8 @@ class Explore extends React.Component {
         <View style={{ flex: 1 }}>
           <View style={{
             height: 100, backgroundColor: 'white',
+            activeBackgroundColor: 'transparent',
+
             borderBottomWidth: 1,
             borderBottomColor: "#dddddd",
           }}>
@@ -72,7 +81,7 @@ class Explore extends React.Component {
 
         </View>
 
-        <ScrollView showsVerticalScrollIndicator={false}>
+        <ScrollView vertical={true} showsVerticalScrollIndicator={false}>
           <View style={{ marginVertical: 120 }}>
             <View >
               <View style={{ justifyContent: 'flex-start', marginLeft: 10, }}>
@@ -82,15 +91,15 @@ class Explore extends React.Component {
                 <ScrollView horizontal={true} showsHorizontalScrollIndicator={false} >
                   <View style={{ flex: 2, borderWidth: 1, borderRadius: 7, borderColor: 'grey', marginLeft: 10 }}>
                     <Image source={require('./assets/images/aeroplane-air-5.jpg')} style={{ width: 140, height: 120, borderTopLeftRadius: 6, borderTopRightRadius: 6 }} />
-                    <Button style={{ width: 110, height: 34, marginTop: 6, justifyContent: 'center', borderColor: 'grey', borderWidth: 1, borderRadius: 7, backgroundColor: '#fffff', alignSelf: 'center' }}>
-                      <Ionicons name="md-planet" size={23} style={{ marginLeft: 28 }} color='grey' />
+                    <Button style={{ tintColor: 'blue', width: 110, height: 34, marginTop: 6, justifyContent: 'center', borderColor: 'grey', borderWidth: 1, borderRadius: 7, backgroundColor: '#fffff', alignSelf: 'center' }}>
+                      <Ionicons name="md-airplane" size={23} style={{ marginLeft: 28 }} color='grey' />
                       <Text style={{ color: "grey", marginRight: 12 }}>Airbus</Text>
                     </Button>
                   </View>
                   <View style={{ shadowColor: '#090000', flex: 2, borderWidth: 1, borderRadius: 7, borderColor: 'grey', marginLeft: 10 }}>
                     <Image source={require('./assets/images/aeroplane-aircraft-1.jpg')} style={{ width: 140, height: 120, borderTopLeftRadius: 6, borderTopRightRadius: 6 }} />
                     <Button style={{ width: 110, height: 34, marginTop: 6, justifyContent: 'center', borderColor: 'grey', borderWidth: 1, borderRadius: 7, backgroundColor: '#fffff', alignSelf: 'center' }}>
-                      <Ionicons name="md-planet" size={23} style={{ marginLeft: 28 }} color='grey' />
+                      <Ionicons name="md-airplane" size={23} style={{ marginLeft: 28 }} color='grey' />
                       <Text style={{ color: "grey", marginRight: 12 }}>Qatar</Text>
                     </Button>
 
@@ -98,7 +107,7 @@ class Explore extends React.Component {
                   <View style={{ shadowColor: '#090000', flex: 2, borderWidth: 1, borderRadius: 7, borderColor: 'grey', marginLeft: 10 }}>
                     <Image source={require('./assets/images/aeroplane-aircraft-4.jpg')} style={{ width: 140, height: 120, borderTopLeftRadius: 6, borderTopRightRadius: 6 }} />
                     <Button style={{ width: 110, height: 34, marginTop: 6, justifyContent: 'center', borderColor: 'grey', borderWidth: 1, borderRadius: 7, backgroundColor: '#fffff', alignSelf: 'center' }}>
-                      <Ionicons name="md-planet" size={23} style={{ marginLeft: 28 }} color='grey' />
+                      <Ionicons name="md-airplane" size={23} style={{ marginLeft: 28 }} color='grey' />
                       <Text style={{ color: "grey", marginRight: 12 }}>Alaska</Text>
                     </Button>
 
@@ -106,7 +115,7 @@ class Explore extends React.Component {
                   <View style={{ shadowColor: '#090000', flex: 2, borderWidth: 1, borderRadius: 7, borderColor: 'grey', marginLeft: 10 }}>
                     <Image source={require('./assets/images/aircraft-aircraft-3.jpg')} style={{ width: 140, height: 120, borderTopLeftRadius: 6, borderTopRightRadius: 6 }} />
                     <Button style={{ width: 110, height: 34, marginTop: 6, justifyContent: 'center', borderColor: 'grey', borderWidth: 1, borderRadius: 7, backgroundColor: '#fffff', alignSelf: 'center' }}>
-                      <Ionicons name="md-planet" size={23} style={{ marginLeft: 28 }} color='grey' />
+                      <Ionicons name="md-airplane" size={23} style={{ marginLeft: 28 }} color='grey' />
                       <Text style={{ color: "grey", marginRight: 12 }}>Etihad</Text>
                     </Button>
 
@@ -114,7 +123,7 @@ class Explore extends React.Component {
                   <View style={{ shadowColor: '#090000', flex: 2, borderWidth: 1, borderRadius: 7, orderColor: 'grey', marginLeft: 10, paddingBottom: 5, marginRight: 10 }}>
                     <Image source={require('./assets/images/aeroplane-aircraft-2.jpg')} style={{ width: 140, height: 120, borderTopLeftRadius: 6, borderTopRightRadius: 6 }} />
                     <Button style={{ width: 110, height: 34, marginTop: 6, justifyContent: 'center', borderColor: 'grey', borderWidth: 1, borderRadius: 7, backgroundColor: '#fffff', alignSelf: 'center' }}>
-                      <Ionicons name="md-planet" size={23} style={{ marginLeft: 28 }} color='grey' />
+                      <Ionicons name="md-airplane" size={23} style={{ marginLeft: 28 }} color='grey' />
                       <Text style={{ color: "grey", marginRight: 12 }}>Indigo</Text>
                     </Button>
 
@@ -136,64 +145,155 @@ class Explore extends React.Component {
                   <Text style={{ fontSize: 18, paddingBottom: 10, fontFamily: 'Roboto', color: 'grey', paddingBottom: 10 }}>Our Airways give worldest best facility</Text>
 
                 </View>
-
-
                 {/*resize mode contain te proper image ne te lavi de che cut nahi thava de*/}
-                <View style={{ justifyContent: 'space-around', alignSelf: 'center', marginVertical: -7 }}>
-                  <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-                    <View style={{ padding: 10, flex: 2 }}>
-                      <ImageBackground source={require('./assets/images/etihad-airways.jpg')} imageStyle={{ borderRadius: 6 }} style={{ width: 340, height: 190 }}>
-                        <View >
-                          <Text style={{ color: "white", marginVertical: 150, marginHorizontal: 90, fontSize: 16 }}>Etihad Airways</Text>
-                        </View>
-                      </ImageBackground>
+                <ScrollView style={{ alignSelf: 'center', marginVertical: -7 }} horizontal={true} showsHorizontalScrollIndicator={false}>
+                  <View style={{ padding: 10, flex: 2 }}>
+                    <ImageBackground source={require('./assets/images/etihad-airways.jpg')} imageStyle={{ borderRadius: 6 }} style={{ width: 340, height: 190 }}>
+                      <Text style={{ borderWidth: 2, borderColor: "#5396df", padding: 5, borderRadius: 6, color: "white", marginVertical: 150, marginHorizontal: 40, textAlign: 'center', fontSize: 16, fontWeight: '600' }}>Etihad Airways</Text>
+                    </ImageBackground>
 
+                  </View>
+                  {/*image set karo and ene scroll karava magta hoy to always width height apo*/}
+                  <View style={{ padding: 10, flex: 2 }}>
+                    <ImageBackground source={require('./assets/images/facility1.jpg')} imageStyle={{ borderRadius: 6 }} style={{ width: 340, height: 190 }}>
+                      <View >
+                        <Text style={{ borderWidth: 2, borderColor: "#5396df", padding: 5, borderRadius: 6, color: "white", marginVertical: 150, marginHorizontal: 40, textAlign: 'center', fontSize: 16, fontWeight: '600' }}>Seating Arrangement</Text>
+                      </View>
+                    </ImageBackground>
+
+                  </View>
+                  <View style={{ padding: 10, flex: 2 }}>
+                    <ImageBackground source={require('./assets/images/facility2.jpg')} imageStyle={{ borderRadius: 6 }} style={{ width: 340, height: 190 }}>
+                      <View >
+                        <Text style={{  borderWidth: 2, borderColor: "#5396df", padding: 5, borderRadius: 6, color: "white", marginVertical: 150, marginHorizontal: 40, textAlign: 'center', fontSize: 16, fontWeight: '600'  }}>Comfortable Seats</Text>
+                      </View>
+                    </ImageBackground>
+
+                  </View>
+                  <View style={{ padding: 10, flex: 2 }}>
+                    <ImageBackground source={require('./assets/images/facility3.jpg')} imageStyle={{ borderRadius: 6 }} style={{ width: 340, height: 190 }}>
+                      <View >
+                        <Text style={{ borderWidth: 2, borderColor: "#5396df", padding: 5, borderRadius: 6, color: "white", marginVertical: 150, marginHorizontal: 40, textAlign: 'center', fontSize: 16, fontWeight: '600'  }}>Eating Facility</Text>
+                      </View>
+                    </ImageBackground>
+
+                  </View>
+                  <View style={{ padding: 10, flex: 2 }}>
+                    <ImageBackground source={require('./assets/images/facility4.jpg')} imageStyle={{ borderRadius: 6 }} style={{ width: 340, height: 190 }}>
+                      <View >
+                        <Text style={{  borderWidth: 2, borderColor: "#5396df", padding: 5, borderRadius: 6, color: "white", marginVertical: 150, marginHorizontal: 40, textAlign: 'center', fontSize: 16, fontWeight: '600'  }}>Sleeping Arrangement</Text>
+                      </View>
+                    </ImageBackground>
+
+                  </View>
+
+                </ScrollView>
+
+                {/* note:if you want to give shadow so you use TouchableOpacity
+so you very easily give shadow otherwise not possible */}
+                {/* start after slider  */}
+                <View style={{ paddingTop: 10, justifyContent: 'flex-start', marginLeft: 10, }}>
+                  <Text style={{ fontSize: 24, paddingBottom: 10, fontFamily: 'Playfair', fontWeight: '600' }}>Places to stay around the worldest best resort</Text>
+                </View>
+                <View style={{ flexDirection: 'row', justifyContent: "space-between", }}>
+
+                  <TouchableOpacity activeOpacity={.7} style={{ flex: 1, margin: 10 }}>
+                    <View >
+                      <View >
+                        <Image source={require('./assets/images/Hotels/1.jpg')} style={{ borderRadius: 7, width: width / 2 - 20, height: height / 4 - 10, resizeMode: 'cover' }} />
+                      </View>
                     </View>
-                    {/*image set karo and ene scroll karava magta hoy to always width height apo*/}
-                    <View style={{ padding: 10, flex: 2 }}>
-                      <ImageBackground source={require('./assets/images/facility1.jpg')} imageStyle={{ borderRadius: 6 }} style={{ width: 340, height: 190 }}>
-                        <View >
-                          <Text style={{ color: "white", marginVertical: 150, marginHorizontal: 60, fontSize: 16 }}>Seating Arrangement</Text>
-                        </View>
-                      </ImageBackground>
-
+                    <Text style={{ fontFamily: 'Roboto', fontWeight: '700', marginTop: 7 }}>Country Hotel in USA</Text>
+                    <Text style={{ fontFamily: 'Roboto', marginTop: 4, width: width / 2, color: 'grey' }}>Green Oasis Bougainvillia in casa</Text>
+                    <Text style={{ fontFamily: 'Roboto', marginTop: 4, width: width / 2, color: 'grey' }}>$900/night</Text>
+                    <View style={{ width: 10, marginTop: 4, }}>
+                      <StarRating
+                        disabled={false}
+                        starSize={10}
+                        maxStars={5}
+                        rating={5}
+                        fullStarColor={'green'}
+                      />
                     </View>
-                    <View style={{ padding: 10, flex: 2 }}>
-                      <ImageBackground source={require('./assets/images/facility2.jpg')} imageStyle={{ borderRadius: 6 }} style={{ width: 340, height: 190 }}>
-                        <View >
-                          <Text style={{ color: "white", marginVertical: 150, marginHorizontal: 90, fontSize: 16 }}>Comfortable Seats</Text>
-                        </View>
-                      </ImageBackground>
+                  </TouchableOpacity>
 
-                    </View>
-                    <View style={{ padding: 10, flex: 2 }}>
-                      <ImageBackground source={require('./assets/images/facility3.jpg')} imageStyle={{ borderRadius: 6 }} style={{ width: 340, height: 190 }}>
-                        <View >
-                          <Text style={{ color: "white", marginVertical: 150, marginHorizontal: 90, fontSize: 16 }}>Eating Facility</Text>
-                        </View>
-                      </ImageBackground>
-
-                    </View>
-                    <View style={{ padding: 10, flex: 2 }}>
-                      <ImageBackground source={require('./assets/images/facility4.jpg')} imageStyle={{ borderRadius: 6 }} style={{ width: 340, height: 190 }}>
-                        <View >
-                          <Text style={{ color: "white", marginVertical: 150, marginHorizontal: 60, fontSize: 16 }}>Sleeping Arrangement</Text>
-                        </View>
-                      </ImageBackground>
-
+                  <TouchableOpacity activeOpacity={.7} style={{ flex: 1, margin: 10 }}>
+                    <View >
+                      <View >
+                        <Image source={require('./assets/images/Hotels/2.jpg')} style={{ borderRadius: 7, width: width / 2 - 20, height: height / 4 - 10, resizeMode: 'cover' }} />
+                      </View>
                     </View>
 
-                  </ScrollView>
 
+                    <Text style={{ fontFamily: 'Roboto', fontWeight: '700', marginTop: 7 }}>Royal Punkla</Text>
+                    <Text style={{ fontFamily: 'Roboto', marginTop: 4, width: width / 2, color: 'grey' }}>seculded Intown Treehouse</Text>
+                    <Text style={{ fontFamily: 'Roboto', marginTop: 4, width: width / 2, color: 'grey' }}>$1200/night</Text>
+                    <View style={{ width: 10, marginTop: 4, }}>
+                      <StarRating
+                        disabled={false}
+                        starSize={10}
+                        maxStars={5}
+                        rating={4}
+                        fullStarColor={'green'}
+                      />
+                    </View>
+                  </TouchableOpacity>
 
                 </View>
+                {/* end slider */}
+                {/* start after slider  */}
 
+                <View style={{ paddingTop: 20, flexDirection: 'row', justifyContent: "space-between", }}>
+
+                  <TouchableOpacity activeOpacity={.7} style={{ flex: 1, margin: 10 }}>
+                    <View >
+                      <View >
+                        <Image source={require('./assets/images/Hotels/3.jpg')} style={{ borderRadius: 7, width: width / 2 - 20, height: height / 4 - 10, resizeMode: 'cover' }} />
+                      </View>
+                    </View>
+                    <Text style={{ fontFamily: 'Roboto', fontWeight: '700', marginTop: 7 }}>Bright Angel Point</Text>
+                    <Text style={{ fontFamily: 'Roboto', marginTop: 4, width: width / 3, color: 'grey' }}>seculded Intown alaska</Text>
+
+                    <Text style={{ fontFamily: 'Roboto', marginTop: 4, width: width / 2, color: 'grey' }}>$1000/night</Text>
+                    <View style={{ width: 10, marginTop: 4, }}>
+                      <StarRating
+                        disabled={false}
+                        starSize={10}
+                        maxStars={5}
+                        rating={4}
+                        halfStar={'ios-star-half'}
+                        fullStarColor={'green'}
+                      />
+                    </View>
+                  </TouchableOpacity>
+
+                  <TouchableOpacity activeOpacity={.7} style={{ flex: 1, margin: 10 }}>
+                    <View >
+                      <View >
+                        <Image source={require('./assets/images/Hotels/4.jpg')} style={{ borderRadius: 7, width: width / 2 - 20, height: height / 4 - 10, resizeMode: 'cover' }} />
+                      </View>
+                    </View>
+
+
+                    <Text style={{ fontFamily: 'Roboto', fontWeight: '700', marginTop: 7 }}>Surf Culb </Text>
+                    <Text style={{ fontFamily: 'Roboto', marginTop: 4, width: width / 2, color: 'grey' }}>seculded Intown Greenhouse</Text>
+                    <Text style={{ fontFamily: 'Roboto', marginTop: 4, width: width / 2, color: 'grey' }}>$1100/night</Text>
+                    <View style={{ width: 10, marginTop: 4, }}>
+                      <StarRating
+                        disabled={false}
+                        starSize={10}
+                        maxStars={5}
+                        rating={3}
+                        fullStarColor={'green'}
+                      />
+                    </View>
+                  </TouchableOpacity>
+
+                </View>
+                {/* end slider */}
               </View>
 
               {/*end*/}
-              <View style={{ flex: 1, marginVertical: 420 }}>
-                <Text>Hello Gajjar Darshit</Text>
-              </View>
             </View>
 
 
@@ -207,8 +307,8 @@ class Explore extends React.Component {
   }
 }
 // export default Explore;
-//tint color is specially is use for when we want to give color 
-//transparent 
+//tint color is specially is use for when we want to give color
+//transparent
 //ex.we make the image and the by default android border became solid
 //so that time tint color through we make the different color
 //ex.we make one icon and some situation icon color will be change so that situation
