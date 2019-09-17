811c657286efb500cc5d656bc8ce3929ffb12863
cismox owner <darshit.hasmukhbhai1998@gmail.com>
2019-09-18 00:06:53 +0530

    hello i am custome

--- App.js
+++ App.js
@@ -1,250 +1,167 @@
-import React from 'react';
-import {
-  CameraRoll,
-  Alert,
-  StyleSheet, Image, ImageBackground,
-  Dimensions, Text, Animated,
-  TouchableOpacity, View, TextInput, ToastAndroid, Vibration, ScrollView
-} from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import { createMaterialTopTabNavigator } from 'react-navigation-tabs';
-import Navigation from './Navigator/Navigation';
-import Trip from './Navigator/Trips.js';
-import Saved from './Navigator/saved';
-import StarRating from 'react-native-star-rating';
-import Icon from 'react-native-vector-icons/Ionicons';
-import IconAnt from 'react-native-vector-icons/AntDesign';
-import IconFont from 'react-native-vector-icons/FontAwesome';
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
-import { RNCamera, FaceDetector } from 'react-native-camera';
-let width = Dimensions.get('window').width;
-let height = Dimensions.get('window').height;
-class Explore extends React.Component {
+import React, { Component } from 'react';
+import { Platform, StyleSheet, View, Text, Modal, Button, TouchableOpacity, Alert } from 'react-native';
+
+export default class Mynewproject extends Component {
+
   constructor(props) {
+
     super(props);
+
     this.state = {
-      color: "grey",
-      photos: []
-    }
-    this.color = this.color.bind(this);
-    this.changeColor = this.changeColor.bind(this);
-    this.barcodeRecognized = this.barcodeRecognized.bind(this);
+
+      Alert_Visibility: false
+    };
+
   }
 
-  color = () => {
+  Show_Custom_Alert(visible) {
 
-    this.setState({
-      color: "red"
-    })
-  }
-  barcodeRecognized = ({ barcodes }) => {
-    barcodes.forEach(barcode => console.warn(barcode.data))
-  };
-  changeColor = () => {
-    if (this.state.color === "red") {
-      console.log("hello");
-      return 1;
-      // kem cho
-    }
-    else {
-      console.log("hello world");
+    this.setState({ Alert_Visibility: visible });
 
-      return 2;
-    }
   }
+
+  ok_Button = () => {
+
+    Alert.alert("OK Button Clicked.");
+
+  }
+
   render() {
+
     return (
-      <ScrollView style={style.header} showsVerticalScrollIndicator={false}>
-        <View >
-          <View >
-            {console.log("gaspar")}
-            <RNCamera
-              ref={ref => {
-                this.camera = ref;
-              }}
-              style={{
-                flex: 1,
-                width: '100%',
-                height: 120
-              }}
-              onGoogleVisionBarcodesDetected={this.barcodeRecognized}
-            >
-            </RNCamera>
+
+      <View style={styles.MainContainer}>
+
+        <Modal
+
+          visible={this.state.Alert_Visibility}
+
+          transparent={false}
+
+          animationType={"fade"}
+
+          onRequestClose={() => { this.Show_Custom_Alert(!this.state.Alert_Visibility) }} >
 
 
-          </View>
-          <View>
-            <ScrollView horizontal={true} showsHorizontalScrollIndicator={false}>
-              <View style={style.Task}>
-                <View style={style.taskname}>
-                  <Text style={{ fontSize: 30, color: "grey", fontFamily: 'Cookie-Regular' }}>Monday</Text>
-                </View>
-              </View>
-              <View style={style.Task}>
+          <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 
-                <View style={style.taskname}>
-                  <Text style={{ fontSize: 30, color: "grey", fontFamily: 'Cookie-Regular' }}>Tuesday</Text>
-                </View>
-              </View>
-              <View style={style.Task}>
-                <View style={style.taskname}>
-                  <Text style={{ fontSize: 28, color: "grey", fontFamily: 'Cookie-Regular' }}>Wendesday</Text>
-                </View>
-              </View>
-              <View style={style.Task}>
-                <View style={style.taskname}>
-                  <Text style={{ fontSize: 30, color: "grey", fontFamily: 'Cookie-Regular' }}>Thursday</Text>
-                </View>
-              </View>
-              <View style={style.Task}>
-                <View style={style.taskname}>
-                  <Text style={{ fontSize: 30, color: "grey", fontFamily: 'Cookie-Regular' }}>Friday</Text>
-                </View>
-              </View>
-              <View style={style.Task}>
-                <View style={style.taskname}>
-                  <Text style={{ fontSize: 30, color: "grey", fontFamily: 'Cookie-Regular' }}>Saturday</Text>
-                </View>
-              </View>
-              <View style={style.Task}>
-                <View style={style.taskname}>
-                  <Text style={{ fontSize: 30, color: "grey", fontFamily: 'Cookie-Regular' }}>Sunday</Text>
-                </View>
-              </View>
-            </ScrollView>
-          </View>
-        </View>
 
-        <View>
-          <View style={style.box}>
-            <TouchableOpacity activeOpacity={0.9} onPress={this.color}>
-              <View style={style.shadow1}>
-                <IconAnt name="API" style={{ color: `${this.state.color}`, fontSize: 40 }} />
-                <Text style={[(this.changeColor() == 1) ? style.color5 : style.color2, style.label]}>Current Task</Text>
+            <View style={styles.Alert_Main_View}>
+
+
+              <Text style={styles.Alert_Title}>Custom Alert Dialog Title.</Text>
+
+
+              <View style={{ width: '100%', height: 2, backgroundColor: '#fff' }} />
+
+
+              <Text style={styles.Alert_Message}> Are You Sure(Alert Dialog Message). </Text>
+
+
+              <View style={{ width: '100%', height: 1, backgroundColor: '#fff' }} />
+
+
+              <View style={{ flexDirection: 'row', height: '30%' }}>
+
+                <TouchableOpacity
+                  style={styles.buttonStyle}
+                  onPress={this.ok_Button}
+                  activeOpacity={0.7}
+                >
+
+                  <Text style={styles.TextStyle}> OK </Text>
+
+                </TouchableOpacity>
+
+                <View style={{ width: 1, height: '100%', backgroundColor: '#fff' }} />
+
+                <TouchableOpacity
+                  style={styles.buttonStyle}
+                  onPress={() => { this.Show_Custom_Alert(!this.state.Alert_Visibility) }}
+                  activeOpacity={0.7}
+                >
+
+                  <Text style={styles.TextStyle}> CANCEL </Text>
+
+                </TouchableOpacity>
+
               </View>
-            </TouchableOpacity>
-            <View style={style.shadow1}>
-              <IconFont name="newspaper-o" style={{ color: "grey", fontSize: 40 }} />
-              <Text style={[(this.changeColor() == 1) ? style.color3 : style.color2, style.label]}>News Feed</Text>
 
             </View>
 
           </View>
-          <View style={style.box2}>
-            <View style={style.shadow1}>
-              <IconAnt name="Trophy" style={{ color: "grey", fontSize: 40 }} />
-              <Text style={[(this.changeColor() == 1) ? style.color4 : style.color2, style.label]}>Reward</Text>
 
-            </View>
-            <TouchableOpacity activeOpacity={0.8} onPress={this.changeColor}>
-              <View style={style.shadow1}>
-                <IconAnt name="piechart" style={{ color: "grey", fontSize: 40, }} />
-                <Text style={[(this.changeColor() == 1) ? style.color1 : style.color2, style.label]}>Task Chart</Text>
 
-              </View>
-            </TouchableOpacity>
+        </Modal>
 
-          </View>
-        </View>
-      </ScrollView>
-    )
+        <Button onPress={() => { this.Show_Custom_Alert(true) }} title="Click Here To Show Custom Alert Dialog" />
+
+
+      </View>
+
+
+    );
   }
 }
-const style = StyleSheet.create({
-  color1: {
-    color: "#8000ff"
-  },
-  color4: {
-    color: "#ff8000"
-  },
-  color3: {
-    color: "#0000ff"
-  },
-  color5: {
-    color: "#2ea5be"
-  },
-  color2: {
-    color: "grey"
-  },
-  header: {
-    height: height / 4,
-  },
-  weatherText: {
-    fontSize: 45,
-    color: "white",
-    fontWeight: 'bold'
-  },
-  weather: {
-    marginLeft: width - 140,
-    marginVertical: -170,
-  },
-  label: {
-    fontSize: 20,
-    marginTop: 20,
-  },
-  Image: {
-    resizeMode: "cover",
-    width: width,
-    height: height / 2 - 60,
+
+const styles = StyleSheet.create({
+
+  MainContainer: {
+
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: (Platform.OS == 'ios') ? 20 : 0
 
   },
-  Task: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    padding: 10,
-    paddingVertical: 30,
-  },
-  box: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-  },
-  box2: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    paddingTop: 20,
-    paddingBottom: 20,
-  },
-  shadow1: {
-    width: width / 2 - 30,
-    height: height / 4,
-    elevation: 6,
-    backgroundColor: "white",
-    borderRadius: 7,
-    justifyContent: "center",
-    alignItems: "center",
+
+  Alert_Main_View: {
+
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: "#009688",
+    height: 200,
+    width: '90%',
     borderWidth: 1,
-    borderColor: "#a851ff"
+    borderColor: '#fff',
+    borderRadius: 7,
 
   },
 
-  taskname: {
-    width: 110,
-    height: 40,
-    backgroundColor: "white",
-    elevation: 4,
-    borderRadius: 14,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  menu: {
-    color: "white",
-    padding: 20
-  },
-  headerText1: {
-    color: "white",
-    fontSize: 35,
-    fontFamily: "Octicons"
-  },
-  headerText2: {
-    color: "white",
-    fontSize: 35,
-    marginTop: 20,
-  },
-  headerView: {
-    paddingLeft: 30,
-    paddingTop: 40
+  Alert_Title: {
 
+    fontSize: 25,
+    color: "#fff",
+    textAlign: 'center',
+    padding: 10,
+    height: '28%'
+
+  },
+
+  Alert_Message: {
+
+    fontSize: 22,
+    color: "#fff",
+    textAlign: 'center',
+    padding: 10,
+    height: '42%'
+
+  },
+
+  buttonStyle: {
+
+    width: '50%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center'
+
+  },
+
+  TextStyle: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 22,
+    marginTop: -5
   }
-})
 
-export default Explore;
+});
